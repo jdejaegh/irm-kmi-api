caa826a9cec08ff57a39a53dc5a890be97141dcf
Jules Dejaeghere <dej.j@live.be>
2025-05-05 22:23:12 +0200

    Re-export public classes

--- irm_kmi_api/__init__.py
+++ irm_kmi_api/__init__.py
@@ -1,1 +1,7 @@
+from .api import IrmKmiApiError, IrmKmiApiCommunicationError, IrmKmiApiClient, IrmKmiApiClientHa
+from .data import Forecast, ConditionEvol, RadarStyle, PollenName, PollenLevel, WarningType, ExtendedForecast, \
+    CurrentWeatherData, WarningData, RadarForecast, RadarAnimationData, AnimationFrameData
+from .pollen import PollenParser
+from .rain_graph import RainGraph
+
 __version__ = '0.1.6'
